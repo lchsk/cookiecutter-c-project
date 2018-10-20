ce22a1563845ef447b71220d55aae5331e8fff19
lchsk <mjlechowski@gmail.com>
2018-10-20 21:58:22 +0100

    Add dependencies

--- {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_name }}.c
+++ {{ cookiecutter.project_name }}/src/{{ cookiecutter.project_name }}.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+{% if cookiecutter.add_glib == 'y' -%}
+#include <glib.h>
+{% endif -%}
+{% if cookiecutter.add_curses == 'y' -%}
+#include <curses.h>
+{% endif %}
 int main(int argc, char** argv)
 {
     printf("Hello world\n");
